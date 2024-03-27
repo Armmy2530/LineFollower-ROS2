d2ff6784900a3e3c8c6addfdb5e5c0f29fedfbd6
Armmy panla <sutigran2557@gmail.com>
2024-03-27 21:23:46 +0700

    fixed gazebo load model (by chaging urdf mesh file path)

--- src/gazebo_simulation/launch/armmygazebo.launch.py
+++ src/gazebo_simulation/launch/armmygazebo.launch.py
@@ -12,8 +12,6 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
@@ -36,24 +34,12 @@ def generate_launch_description():
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
                     # launch_arguments={'world': world}.items()
              )
-    # gzserver = IncludeLaunchDescription(
-    #         PythonLaunchDescriptionSource(
-    #             os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-    #         ),
-    #         launch_arguments={'world': world}.items(),
-    #     )
 
-    # gzclient = IncludeLaunchDescription(
-    #         PythonLaunchDescriptionSource(
-    #             os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-    #         ),
-    #     )
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=["-topic", "robot_description", "-entity", "robot", "-x", "0.0", "-y", "0.0", "-z", "0.0"],    
+                        arguments=["-topic", "robot_description", "-entity", "robot"],    
+                        # arguments=["-topic", "robot_description", "-entity", "robot", "-x", "0.0", "-y", "0.0", "-z", "0.0"],    
                         output='screen')
-
-
 
     # Launch them all!
     return LaunchDescription([
