521e70dd9c66f5152f831e93f2bdc242dd857f51
Armmy panla <sutigran2557@gmail.com>
2024-04-04 00:10:24 +0700

    testing opencv image pub and sub

---
+++ src/line_follower/line_follower/image_pub.py
@@ -0,0 +1,49 @@
+import rclpy # Python Client Library for ROS 2
+from rclpy.node import Node # Handles the creation of nodes
+from sensor_msgs.msg import Image # Image is the message type
+from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+import cv2 # OpenCV library
+ 
+class ImagePublisher(Node):
+    def __init__(self):
+        super().__init__('image_pub')
+        
+        self.publisher_ = self.create_publisher(Image, 'test_image', 10)
+
+        # Create a VideoCapture object
+        # The argument '0' gets the default webcam.
+        self.img = cv2.imread('line_follower/data/test_data/1.png')
+        
+        # Used to convert between ROS and OpenCV images
+        self.br = CvBridge()
+        
+        self.test_pub()
+        
+    
+    def test_pub(self):
+        cv2.imshow('image',self.img)
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
+
+        self.publisher_.publish(self.br.cv2_to_imgmsg(self.img))
+        self.get_logger().info('Publishing image')
+
+
+def main(args=None):
+  
+  # Initialize the rclpy library
+  rclpy.init(args=args)
+  # Create the node
+  image_publisher = ImagePublisher()
+  # Spin the node so the callback function is called.
+  rclpy.spin(image_publisher)
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
+  image_publisher.destroy_node()
+  
+  # Shutdown the ROS client library for Python
+  rclpy.shutdown()
+  
+if __name__ == '__main__':
+  main()
