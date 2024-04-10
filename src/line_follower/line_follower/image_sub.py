0c2a54ddf74048659b2af011445bf3f5dbdd3a4b
Armmy panla <sutigran2557@gmail.com>
2024-04-10 22:20:51 +0700

    finish public error to ros topic

--- src/line_follower/line_follower/image_sub.py
+++ src/line_follower/line_follower/image_sub.py
@@ -11,7 +11,8 @@ from sensor_msgs.msg import Image # Image is the message type
 from geometry_msgs.msg      import Point
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 import cv2 # OpenCV library
-import line_follower.image_process as line_impf
+import line_follower.image_process as line_imp
+from line_interfaces.msg import LineError
  
 class ImageSubscriber(Node):
   """
@@ -25,7 +26,7 @@ class ImageSubscriber(Node):
     super().__init__('image_subscriber')
 
     self.image_pub = self.create_publisher(Image, 'camera/line2p_image', 10)
-    self.line_pub = self.create_publisher(Point, 'line/error_position' ,10)
+    self.line_pub = self.create_publisher(LineError, 'line/error_position' ,10)
 
     # Create the subscriber. This subscriber will receive an Image
     # from the video_frames topic. The queue size is 10 messages.
@@ -38,35 +39,44 @@ class ImageSubscriber(Node):
       
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
+    
+    # Line follower variable
+    self.last_error = -1
+    self.found_line = False
    
   def listener_callback(self, data):
     """
     Callback function.
     """
-    # Display the message on the console
-    self.get_logger().info('Receiving video frame')
  
     # Convert ROS Image message to OpenCV image
     current_frame = self.br.imgmsg_to_cv2(data)
     
+    h, w , _ = current_frame.shape
     centerline_point = line_imp.find_point(current_frame,[400])
     if(centerline_point[0][0] != -1):
       centerline_draw_img = line_imp.drawimg_point(current_frame , centerline_point)
+      error_fromcenter = centerline_point[0][2] - (w/2)
+      self.last_error = error_fromcenter
+      self.found_line = True
     else:
       centerline_draw_img = current_frame
+      error_fromcenter = -1.0
+      self.found_line = False
+
     # centerline_draw_img = line_imp.draw_centerline(current_frame)
 
     img_pub = self.br.cv2_to_imgmsg(centerline_draw_img,encoding='rgb8')
     img_pub.header = data.header
 
-    line_msg = Point()
-    line_msg.x = float(centerline_point[0][0])
-    line_msg.y = float(centerline_point[0][1])
-    line_msg.z = float(centerline_point[0][2])
+    line_error = LineError() 
+    line_error.current_error = error_fromcenter
+    line_error.last_error = self.last_error
+    line_error.line_found = self.found_line
+    self.get_logger().info(f"Publishing error msg: {self.found_line} {error_fromcenter} {self.last_error}")
 
     self.image_pub.publish(img_pub)
-    self.line_pub.publish(line_msg)
-    self.get_logger().info('Publishing image')
+    self.line_pub.publish(line_error)
   
 def main(args=None):
   
